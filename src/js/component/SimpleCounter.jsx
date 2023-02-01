010a2bc3f7cdbb201f5f518facef75b18eaad0bc
Crv86 <lil_criss69@hotmail.com>
2023-02-01 21:13:54 +0000

    Nuevo

---
+++ src/js/component/SimpleCounter.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const SimpleCounter = (props) => {
+    return(
+        <div className="BigDiv">
+            <div><i class="fas fa-stopwatch"></i></div>
+            <div>{Math.floor(props.number / 100000 % 10)}</div>
+            <div>{Math.floor(props.number / 10000 % 10)}</div>
+            <div>{Math.floor(props.number / 1000 % 10)}</div>
+            <div>{Math.floor(props.number / 100 % 10)}</div>
+            <div>{Math.floor(props.number / 10 % 10)}</div>
+            <div>{props.number % 10}</div>
+        </div>
+
+    )
+}
+SimpleCounter.propTypes = {
+    number:PropTypes.number
+};
+
+SimpleCounter.defaultProps = {
+    number:0
+};
+
+export default SimpleCounter;
